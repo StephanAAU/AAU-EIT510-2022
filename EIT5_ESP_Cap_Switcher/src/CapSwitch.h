b3baffc9958b95456137b4f6a52d612286ec4d86
Thegaardenator <sandergaarden@gmail.com>
2022-12-05 10:03:50 +0100

    Fixed gpio fuckery

--- EIT5_ESP_Cap_Switcher/src/CapSwitch.h
+++ EIT5_ESP_Cap_Switcher/src/CapSwitch.h
@@ -6,6 +6,7 @@
 #include <esp32-hal-ledc.h>
 #include <Esp.h>
 #include <stdint.h>
+#include <Preferences.h>
 
 #ifndef CapSwitch_h
 #define CapSwitch_h
@@ -16,21 +17,26 @@ class CapSwitch {
         static const uint8_t BASE_CAP_AMOUNT = 1;
         static const uint8_t BASE_PIN_START = 30;
 
-        /* Variables set by constructor */
-        uint8_t _cap_amount;
-        uint8_t _pin_start;
+        
 
         /* Another base variable */
-        bool _single_pin_state = false;
+        
 
         int FindTableIndex(uint8_t val);
         int FindTestTableIndex(uint8_t val);
 
     public:
+        /* Variables set by constructor */
+        uint8_t _cap_amount;
+        uint8_t _pin_start;
+        
         /* Variables */
+        String _pref_name;
+        bool _single_pin_state = false;
 
         /* Functions */
-        CapSwitch(uint8_t cap_amount = BASE_CAP_AMOUNT, uint8_t pin_start = BASE_PIN_START);
+        // CapSwitch(uint8_t cap_amount = BASE_CAP_AMOUNT, uint8_t pin_start = BASE_PIN_START);
+        CapSwitch(uint8_t cap, String prefName);
         int ClusterStaticSwitch(uint8_t capState);
         int SingleStaticSwitch(uint8_t capState);
         int SinglePWMSwitch(uint8_t pwmVal);
--- EIT5_ESP_Cap_Switcher/src/main.cpp
+++ EIT5_ESP_Cap_Switcher/src/main.cpp
@@ -5,12 +5,50 @@
 #include <CapSwitch.h>
 
 /* Set up cap switches */
-CapSwitch switch1(4, 32); // Pins 34-35 do not work on my board
-CapSwitch switch2(4, 16);
-CapSwitch switchTrim; // THIS IS A SINGLE PIN AT PIN 30
-const int switchArrSize = 3;
-CapSwitch switchArr[switchArrSize] = {switch1, switch2, switchTrim};
+// CapSwitch switch1(4, 32); // Pins 34-35 do not work on my board
+// CapSwitch switch2(4, 16);
+// CapSwitch switchTrim; // THIS IS A SINGLE PIN AT PIN 30
+// const int switchArrSize = 3;
+// CapSwitch switchArr[switchArrSize] = {switch1, switch2, switchTrim};
 
+CapSwitch sw0(18, "sw0");
+CapSwitch sw1(5, "sw1");
+CapSwitch sw2(17, "sw2");
+CapSwitch sw3(16, "sw3");
+
+CapSwitch sw4(23, "sw4");
+CapSwitch sw5(22, "sw5");
+CapSwitch sw6(21, "sw6");
+CapSwitch sw7(19, "sw7");
+
+CapSwitch swTune(13, "swTune");
+
+const int switchArrSize = 9;
+CapSwitch switchArr[switchArrSize] = {sw0, sw1, sw2, sw3, sw4, sw5, sw6, sw7, swTune};
+
+Preferences prefs;
+char prefNameSpace[] = "defaultPins";
+
+int PutPreference(CapSwitch sw) {
+    char prefArr[sw._pref_name.length() + 1];
+    strcpy(prefArr, sw._pref_name.c_str());
+
+    prefs.putBool(prefArr, sw._single_pin_state);
+
+    return 0;
+}
+
+int GetPreference(CapSwitch sw) {
+    char prefArr[sw._pref_name.length() + 1];
+    strcpy(prefArr, sw._pref_name.c_str());
+
+    bool state = prefs.getBool(prefArr, false);
+
+    digitalWrite((sw._pin_start), state);
+    sw._single_pin_state = state;
+
+    return 0;
+}
 
 void setup() {
     Serial.begin(115200);
@@ -18,6 +56,12 @@ void setup() {
     Serial.println("Setup happening");
 
     ledcSetup(1, 12000, 8);
+
+    prefs.begin(prefNameSpace, false);
+
+    for(int i = 0; i < switchArrSize; i++) {
+        GetPreference(switchArr[i]);
+    }
 
     Serial.begin(115200);
     Serial.println("Ready to receive\n");
@@ -32,21 +76,11 @@ void loop() {
         int cluster    = Serial.readStringUntil(',').toInt();
         int state      = Serial.readStringUntil('\n').toInt();
 
-        // if(cluster == 0 || state == 0) {return;}
-
         if(cluster < 0 || cluster > switchArrSize) {
             Serial.printf("Error: Cluster number out of bounds\n");
             return;
         }
-        if(command.equals("static")) {
-            if(switchArr[cluster].ClusterStaticSwitch(state) == 0) {
-                Serial.printf("Switch cluster %i changed to static state %i\n", cluster, state);
-            }
-            else {
-                Serial.printf("Something went wrong\n");
-            }
-        }
-        else if(command.equals("sinagle")) {
+        else if(command.equals("single")) {
             if(state < 0 || state > 1) {
                 Serial.printf("Error: State is not 0 or 1\n");
                 return;
@@ -58,30 +92,77 @@ void loop() {
                 Serial.printf("Something went wrong\n");
             }
         }
-        else if(command.equals("pwm")) {
-            if(state < 0 || state > 255) {
-                Serial.printf("Error: Duty cycle is not between 0 and 100\n");
-                return;
+        else if(command.equals("save")) {
+            for(int i = 0; i < switchArrSize; i++) {
+                PutPreference(switchArr[i]);
             }
-            if(switchArr[cluster].SinglePWMSwitch(state) == 0) {
-                Serial.printf("Switch %d changed to duty cycle %d%%\n", cluster, state);
-            }
-            else {
-                Serial.printf("Something went wrong\n");
-            }
-        }
-        else if(command.equals("test")) {
-            if(switchArr[cluster].TestSwitches() == 0) {
-                Serial.printf("Switches tested\n");
-            }
-            else {
-                Serial.printf("Something went wrong\n");
-            }
+            Serial.printf("Setup saved");
         }
 
         else {
             Serial.printf("Invalid command\n");
         }
     }
-
 }
+
+// void loop() {
+//     if(Serial.available()) {
+//         // Form will be *,*,*
+
+//         String command = Serial.readStringUntil(',');
+//         int cluster    = Serial.readStringUntil(',').toInt();
+//         int state      = Serial.readStringUntil('\n').toInt();
+
+//         // if(cluster == 0 || state == 0) {return;}
+
+//         if(cluster < 0 || cluster > switchArrSize) {
+//             Serial.printf("Error: Cluster number out of bounds\n");
+//             return;
+//         }
+//         if(command.equals("static")) {
+//             if(switchArr[cluster].ClusterStaticSwitch(state) == 0) {
+//                 Serial.printf("Switch cluster %i changed to static state %i\n", cluster, state);
+//             }
+//             else {
+//                 Serial.printf("Something went wrong\n");
+//             }
+//         }
+//         else if(command.equals("single")) {
+//             if(state < 0 || state > 1) {
+//                 Serial.printf("Error: State is not 0 or 1\n");
+//                 return;
+//             }
+//             if(switchArr[cluster].SingleStaticSwitch(state) == 0) {
+//                 Serial.printf("Switch %i changed to static state %i\n", cluster, state);
+//             }
+//             else {
+//                 Serial.printf("Something went wrong\n");
+//             }
+//         }
+//         else if(command.equals("pwm")) {
+//             if(state < 0 || state > 255) {
+//                 Serial.printf("Error: Duty cycle is not between 0 and 100\n");
+//                 return;
+//             }
+//             if(switchArr[cluster].SinglePWMSwitch(state) == 0) {
+//                 Serial.printf("Switch %d changed to duty cycle %d%%\n", cluster, state);
+//             }
+//             else {
+//                 Serial.printf("Something went wrong\n");
+//             }
+//         }
+//         else if(command.equals("test")) {
+//             if(switchArr[cluster].TestSwitches() == 0) {
+//                 Serial.printf("Switches tested\n");
+//             }
+//             else {
+//                 Serial.printf("Something went wrong\n");
+//             }
+//         }
+
+//         else {
+//             Serial.printf("Invalid command\n");
+//         }
+//     }
+
+// }
